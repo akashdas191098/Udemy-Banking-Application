a2071c844e8d7c2c6bafe097f46ba72bc9bdcee7
Akash <akashdas191098@gmail.com>
2025-08-13 13:12:35 +0530

    added controller

---
+++ Udemy-Banking-application/accounts/src/main/java/com/eazybytes/accounts/controllers/AccountController.java
@@ -0,0 +1,11 @@
+package com.eazybytes.accounts.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+
+
+}
